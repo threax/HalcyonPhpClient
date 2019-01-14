10b4d047f72f57c7381456e3f48b3832fb86f1cc
Andrew Piper <piper.andrew@spcollege.edu>
2019-01-14 08:38:04 -0500

    Load bearer token in access token extension.

--- index.php
+++ index.php
@@ -1,34 +1,14 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Jumbojett\OpenIDConnectClient;
 use threax\halcyonclient\HalEndpointClient;
 use threax\halcyonclient\CurlHelper;
 use threax\halcyonclient\AccessTokenCurlExtension;
 
 $client = new HalEndpointClient();
 
-$idServerHost = 'https://localhost:44390';
-
-$oidc = new OpenIDConnectClient($idServerHost,
-                                'PHPTest',
-                                'notyetdefined');
-
-//For dev
-$oidc->setVerifyHost(false);
-$oidc->setVerifyPeer(false);
-
-//For prod
-//$oidc->setCertPath(__DIR__ . '/certs/cacert.pem');
-
-$oidc->providerConfigParam(array('token_endpoint'=> $idServerHost . '/connect/token'));
-$oidc->addScope('Spc.Authority');
-
-// this assumes success (to validate check if the access_token property is there and a valid JWT) :
-$clientCredentialsToken = $oidc->requestClientCredentialsToken()->access_token;
-//echo $clientCredentialsToken;
-
-$bearerExt = new AccessTokenCurlExtension($clientCredentialsToken);
+$bearerExt = new AccessTokenCurlExtension('https://localhost:44390', 'PHPTest', 'notyetdefined', 'Spc.Authority');
+$bearerExt->setIgnoreCertErrors(true);
 
 $curlHelper = new CurlHelper();
 $curlHelper->addRequestExtension($bearerExt);
--- src/AccessTokenCurlExtension.php
+++ src/AccessTokenCurlExtension.php
@@ -2,18 +2,55 @@
 
 namespace threax\halcyonclient;
 
+use Jumbojett\OpenIDConnectClient;
 use threax\halcyonclient\ICurlRequestExtension;
 
 class AccessTokenCurlExtension implements ICurlRequestExtension {
-    private $bearer;
+    private $idServerHost;
+    private $clientId;
+    private $clientSecret;
+    private $ignoreCertErrors = false;
+    private $scopes;
+    private $token = null;
 
-    public function __construct(string $bearer){
-        $this->bearer = $bearer;
+    public function __construct(string $idServerHost, string $clientId, string $clientSecret, string ...$scopes){
+        $this->idServerHost = $idServerHost;
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->scopes = $scopes;
+    }
+
+    public function getIgnoreCertErrors(): bool {
+        return $this->ignoreCertErrors;
+    }
+
+    public function setIgnoreCertErrors(bool $value): void {
+        $this->ignoreCertErrors = $value;
     }
 
     public function addConfig($curl) {
+        if($this->token === null) {
+            $oidc = new OpenIDConnectClient($this->idServerHost, $this->clientId, $this->clientSecret);
+
+            if($this->ignoreCertErrors) {
+                $oidc->setVerifyHost(false);
+                $oidc->setVerifyPeer(false);
+            }
+            else {
+                $oidc->setCertPath(__DIR__ . '/../certs/cacert.pem');
+            }
+
+            $oidc->providerConfigParam(array('token_endpoint'=> $this->idServerHost . '/connect/token'));
+            foreach ($this->scopes as $scope) {
+                $oidc->addScope($scope);
+            }
+
+            // this assumes success (to validate check if the access_token property is there and a valid JWT) :
+            $this->token = $oidc->requestClientCredentialsToken()->access_token;
+        }
+
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            'bearer: ' . $this->bearer
+            'bearer: ' . $this->token
         ));
     }
 }
